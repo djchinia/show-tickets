1ff2b74fbbe3df7b7e6cd6d021d61e1afe88abba
djchinia <djchinia@gmail.com>
2019-09-10 12:00:51 -0400

    cards formatted

--- src/components/Event.js
+++ src/components/Event.js
@@ -5,6 +5,7 @@ import {
   CardActions,
   CardContent,
   Typography,
+  CardMedia,
 } from '@material-ui/core';
 
 const Event = props => {
@@ -12,23 +13,34 @@ const Event = props => {
   return (
     <div>
       {props.event && (
-        <Card style={{ height: 240 }}>
+        <Card>
+          <CardMedia
+            style={{ height: 0, paddingTop: '70%' }}
+            image="https://img.vggcdn.net/img/cat/4508/1/37.jpg"
+            title={props.event.EventName}
+          />
           <CardContent>
             <Typography gutterBottom variant="headline" component="h2">
-              {props.event.Date}
+              {props.event.EventName}
             </Typography>
-            <Typography component="p">{props.event.EventName}</Typography>
+            <Typography component="p">{props.event.VenueCity}</Typography>
+            <Typography component="p">{props.event.VenueName} </Typography>
+            <Typography component="p">
+              {props.event.Day}, {props.event.Date}
+              {' @ '}
+              {props.event.Time}
+            </Typography>
+            <CardActions style={{ justifyContent: 'center' }}>
+              <Button
+                size="small"
+                color="primary"
+                href={'https://www.viagogo.com/' + props.event.EventUrl}
+                target="_blank"
+              >
+                Buy Tickets
+              </Button>
+            </CardActions>
           </CardContent>
-          <CardActions>
-            <Button
-              size="small"
-              color="primary"
-              href={props.event.MinPrice}
-              target="_blank"
-            >
-              Go To Event
-            </Button>
-          </CardActions>
         </Card>
       )}
     </div>
