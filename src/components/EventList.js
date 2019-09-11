19ab3fb73356ca346097205def226f385bce682f
djchinia <djchinia@gmail.com>
2019-09-11 10:40:33 -0400

    functional select showing city name

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,41 +1,69 @@
 import React, { Component } from 'react';
-import { Grid, Select, MenuItem } from '@material-ui/core';
+import { Grid, Select, MenuItem, FormControl, Button } from '@material-ui/core';
 import Event from './Event';
 import venueListMaker from '../utils/venueListMaker';
 import venueObjFilter from '../utils/venueObjFilter';
 import json from '../utils/eventlist';
 
-const allEvents = json['Items'];
-
 class EventList extends Component {
   constructor() {
     super();
     this.state = {
-      events: allEvents,
-      selectedCity: '',
+      events: json['Items'], // initialize with all events
+      city: '',
       inputStr: '',
+      open: false,
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ city: event.target.value });
+  }
+
+  handleClose() {
+    this.setState({ open: false });
+  }
+
+  handleOpen() {
+    this.setState({ open: true });
   }
 
   render() {
     console.log(venueObjFilter('Houston'));
     const venueList = venueListMaker();
+
     return (
       <div>
         {this.state.events ? (
           <div>
-            <Select
-              value={this.state.selectedCity}
-              //   onChange={handleChange}
-              inputProps={{
-                name: 'city',
-                id: 'city-simple',
-              }}
-            >
-              {venueList.map(singleVenue => (
-                <MenuItem value={singleVenue}>{singleVenue}</MenuItem>
-              ))}
-            </Select>
+            <form autoComplete="off">
+              <br />
+              <FormControl>
+                <Button onClick={this.handleOpen}>Choose a City</Button>
+
+                <Select
+                  open={this.state.open}
+                  onClose={this.handleClose}
+                  onOpen={this.handleOpen}
+                  value={this.state.city}
+                  onChange={this.handleChange}
+                  inputProps={{
+                    name: 'city',
+                    id: 'select-city',
+                  }}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  {venueList.map(singleCity => (
+                    <MenuItem value={singleCity}>{singleCity}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </form>
             <Grid container style={{ padding: 40 }}>
               {this.state.events.map(singleEvent => (
                 <Grid style={{ padding: 20 }} item xs={12} sm={6} lg={4} xl={3}>
