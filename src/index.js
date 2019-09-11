9d69040925cef7341ada3bf71292c87a6dba7e9b
djchinia <djchinia@gmail.com>
2019-09-11 16:54:54 -0400

    some styling and attempted test

--- src/index.js
+++ src/index.js
@@ -3,28 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
-import { lightBlue, lightGreen } from '@material-ui/core/colors';
+// import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+// import { lightBlue, lightGreen } from '@material-ui/core/colors';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: lightBlue,
-    secondary: {
-      main: lightGreen.A400,
-      light: lightBlue[200],
-      dark: lightGreen[700],
-    },
-    type: 'dark',
-  },
-  spacing: {
-    unit: 10,
-  },
-});
+// currently commented out theme since select is not visible with styles
+
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: lightBlue,
+//     secondary: {
+//       main: lightGreen.A400,
+//       light: lightBlue[200],
+//       dark: lightGreen[700],
+//     },
+//     type: 'dark',
+//   },
+//   spacing: {
+//     unit: 10,
+//   },
+// });
 
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
-    <App />
-  </MuiThemeProvider>,
+  // <MuiThemeProvider theme={theme}>
+  <App />,
+  // </MuiThemeProvider>,
   document.getElementById('root')
 );
 
